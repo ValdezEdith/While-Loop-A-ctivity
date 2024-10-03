4c3cea41ebe696f589582eb525ba6c7fa5bb5144
ValdezEdith <zedlavhtide@gmail.com>
2024-10-03 22:33:11 +0800

    Update numberGuess.js

--- numberGuess.js
+++ numberGuess.js
@@ -1,18 +1,12 @@
-
 let secretNumber = Math.floor(Math.random() * 10) + 1;
-
 let guess =("9");
 
 guess = (guess)
-
 while (guess !== secretNumber) {
-  
   if (guess > secretNumber) 
     {console.log("Too high!");} 
   else 
   {console.log("Too low!");}
-
-  
   guess = ("1");
   guess = parseInt(guess);}
 
